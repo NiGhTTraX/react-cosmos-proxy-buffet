e3449dbbf64fca2f3e407509aa6dc69883d0eff3
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-08-31 21:52:35 +0200

    Make the bar collapsible

--- src/proxy-bar.tsx
+++ src/proxy-bar.tsx
@@ -1,4 +1,5 @@
 import React, { Component, ComponentType } from 'react';
+import classNames from 'classnames';
 import './proxy-bar.less';
 
 export interface ProxyIconProps {
@@ -14,14 +15,44 @@ export interface ProxyBarProps {
   proxies: Proxy[]
 }
 
-export default class ProxyBar extends Component<ProxyBarProps> {
+interface ProxyBarState {
+  collapsed: boolean;
+}
+
+export default class ProxyBar extends Component<ProxyBarProps, ProxyBarState> {
+  state = {
+    collapsed: false
+  };
+
   render() {
     const { proxies } = this.props;
+    const classes = classNames('proxy-bar bottom-right', {
+      expanded: !this.state.collapsed
+    });
 
-    return <ul className="proxy-bar bottom-right">
-      {proxies.map(({ Icon, name }) => <li key={name}>
-        <Icon />
-      </li>)}
-    </ul>;
+    return <div className={classes}>
+      <button className="toggle" onClick={this.onToggle}>
+        <svg width={10} height={10} viewBox="0 0 1792 1792">
+          {this.state.collapsed ? <path
+            d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z"
+            fill="#fff"
+          /> : <path
+            d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z"
+            fill="#fff"
+          />}
+        </svg>
+      </button>
+      <ul>
+        {proxies.map(({ Icon, name }) => <li key={name}>
+          <Icon />
+        </li>)}
+      </ul>
+    </div>;
   }
+
+  private onToggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  };
 }
--- tests/unit/proxy-bar.spec.tsx
+++ tests/unit/proxy-bar.spec.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Simulate } from 'react-dom/test-utils';
 import { createReactStub } from 'react-mock-component';
 import { describe, it, $render, expect, beforeEach } from './suite';
 import ProxyBar, { Proxy, ProxyIconProps } from '../../src/proxy-bar';
@@ -30,9 +31,19 @@ describe('ProxyBar', () => {
     expect($proxyBar.hasClass('bottom-right')).to.be.true;
   });
 
+  it('should render by default expanded', () => {
+    expect($proxyBar.hasClass('expanded')).to.be.true;
+  });
+
   it('should render the icons for all proxies', () => {
     expect($proxyBar.text())
       .to.contain('icon 1')
       .and.to.contain('icon 2');
+  });
+
+  it('should collapse', () => {
+    Simulate.click($proxyBar.find('.toggle')[0]);
+
+    expect($proxyBar.hasClass('expanded')).to.be.false;
   });
 });
