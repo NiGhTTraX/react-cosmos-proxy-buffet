9db6ac25b0f530c07a54eaed3de61fd974b98ad1
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-09-01 17:52:46 +0200

    Pass all the props in fixture

--- playground/fixtures/proxy-bar/base.tsx
+++ playground/fixtures/proxy-bar/base.tsx
@@ -1,9 +1,17 @@
-import ProxyBar, { ProxyBarProps } from '../../../src/proxy-bar';
+import ProxyBar, { IStorage, ProxyBarProps } from '../../../src/proxy-bar';
 import DocsProxy from '../../proxies/docs-proxy';
+
+const blackHoleStorage: IStorage = {
+  get: () => null,
+  set: () => {}
+};
+
+const props: ProxyBarProps = {
+  proxies: [DocsProxy],
+  storage: blackHoleStorage
+};
 
 export default {
   component: ProxyBar,
-  props: {
-    proxies: [DocsProxy]
-  } as ProxyBarProps
+  props
 };
