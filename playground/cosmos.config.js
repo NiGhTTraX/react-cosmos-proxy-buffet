8642f94e82abb34755e67874e509b8fedb34a6c9
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-09-07 23:11:56 +0200

    Unify dev and acceptance playgrounds
    Possible due to new fixtures with all possible positions for the bar.

--- playground/cosmos.config.js
+++ playground/cosmos.config.js
@@ -4,13 +4,7 @@ module.exports = {
   fileMatch: ['**/fixtures/**/*.{ts,tsx}'],
   watchDirs: ['src', 'playground'],
 
-  // We don't want to include the proxy in other cases because
-  // it would overlap the components that make it.
-  exclude: process.env.RCPB_ACCEPTANCE ? [] : [/fixtures\/playground/],
-  proxiesPath: process.env.RCPB_ACCEPTANCE
-    ? 'playground/cosmos.proxies.js'
-    : 'something invalid',
-
+  proxiesPath: 'playground/cosmos.proxies.js',
   webpackConfigPath: 'webpack.config.js',
 
   hostname: '0.0.0.0',
